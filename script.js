b109172056762403a3a5f41a2ac8a37a83e80a89
HamBlah <105314803+HamBlah@users.noreply.github.com>
2025-03-25 01:05:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    console.log("Hello from JavaScript!");
+    const iframe = document.querySelector("iframe");
+    iframe.style.border = "5px solid red"; // Example of styling the iframe
+};
